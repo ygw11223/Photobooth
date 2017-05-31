9a46fa43a1d5e48f73a11424c2cda462f95215b6
thecatluver <jendou0707@gmail.com>
2017-05-30 18:26:37 -0700

    fixed mobile splash page

--- photobooth.js
+++ photobooth.js
@@ -1,12 +1,14 @@
+var port = 10008;
+
 function redirect() {
-    var url = "http://138.68.25.50:10008/photoboothMain.html";
+    var url = "http://138.68.25.50:"+ port + "/photoboothMain.html";
     window.location.href=url;
 }
 
 function mobile(){
 	if(document.documentElement.clientWidth <= 480)
 	{
-		var url = "http://138.68.25.50:10008/photo/photoboothMain.html";
+		var url = "http://138.68.25.50:"+ port + "/photoboothMain.html";
     	window.location.replace(url);
 	}
 }
